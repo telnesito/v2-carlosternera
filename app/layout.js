164b86b5304ce2291b9a82b587a5c07daba2b440
Telnesito <carlosj_1703@hotmail.com>
2024-06-13 01:22:28 -0400

    style: :art: Se agrego el favicon a la pagina

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="logotelne.svg" />
       <body className={`${inter.className} bg-[#0a192f] `}>{children}</body>
     </html>
   );
